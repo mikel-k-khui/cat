1777dd0aa20c2dd36c1015faf713f3f336c2e771
Mike Chui <mikel.k.khui@gmail.com>
2019-07-30 20:58:09 -0600

    Exercise to practice more callbacks to return values

---
+++ asyncBreeds.js
@@ -0,0 +1,20 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    // Returning from inner callback function.
+    console.log('Callback: I have the data!: ');
+    (!error ? callback(data) : callback(undefined));
+  });
+  // ISSUE: Attempting to return data out here will also not work.
+  //        Currently not returning anything from here, so this function returns undefined.
+  // console.log("End of breedDetailsFromFile");
+};
+
+// we try to get the return value
+breedDetailsFromFile('Bombay', (catBreed) => {
+  console.log('Return Value: ', catBreed);
+});
